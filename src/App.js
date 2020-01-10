28717288ffec6b2010344411a417d703c5d091ff
ArrthurGeronimo <artha.desenvolvimento@gmail.com>
2020-01-10 15:53:01 -0400

    Hello World

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return ( 
+    <div style={{ width: '100%', height: '100vh', backgroundColor: 'orange', display: 'flex', alignContent: 'center', alignItems: 'center' }}>
+      <h1 style={{ margin: '0px auto', fontSize: '5em'}}>FUNCIONOU!</h1>
+    </div>
+   );
+}
+ 
+export default App;
